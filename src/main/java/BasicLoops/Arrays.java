8117475146c6bd9ce73445c6d3a15178c236fe77
aisabel <anarodriguez0333@gmail.com>
2023-08-30 14:04:40 +0200

    Add files via upload

--- src/main/java/BasicLoops/Arrays.java
+++ src/main/java/BasicLoops/Arrays.java
@@ -5,19 +5,20 @@ public class Arrays {
     public static void main (String[] args) {
 
         //Array is a container which stores multiple values of the same datatype. Collection of values.
-        //Single dimensions array described
+        //Single dimensions array described below:
 
         System.out.println("Single dimension array with memory allocation: ");
 
         //Below traditional array declaration (a) and allocate memory for the values
-        int[] a = new int[4]; //By adding number 4 means array size (quantity of spaces will be set up in memory)
+        //By adding number 4 means array size (quantity of spaces will be set up in memory)
+        int[] a = new int[4];
         a[0] = 1;
         a[1] = 2;
         a[2] = 3;
         a[3] = 4;
 
         //For loop is an incremental check the size of the array and store it in variable i
-        for(int i=0; i<a.length ; i++){
+        for (int i=0; i<a.length ; i++){
             //Prints the content of array (a) in the position i
             System.out.println(a[i]);
         }
@@ -29,7 +30,7 @@ public class Arrays {
         int [] b = {6,7,8,9,10};
 
         //For loop is an incremental check the size of the array and store it in variable i
-        for(int i=0; i<b.length ; i++){
+        for (int i=0; i<b.length ; i++){
             //Prints the content of array b in the position i
             System.out.println(b[i]);
         }
@@ -59,11 +60,20 @@ public class Arrays {
                 System.out.print(c[i][j]);
                 System.out.print("\t"); //prints a space between numbers
             }
-            //Prinln jump to the next row
+            //println jump to the next row
             System.out.println("");
         }
 
         //Prints the value in that specific position = 3
-        System.out.println("Multidimensional array value in second row, first column: "+c[1][0]);
+        System.out.println("Example: Multidimensional array value in second row, first column: "+c[1][0]);
+
+        //Prints Multidimensional array within their positions
+        System.out.println("Multidimensional array values within their positions");
+        int[][] abc = {{1, 2, 3}, {4, 5, 6}, {9, 8, 9}};
+        for (int i = 0; i < abc.length; i++) {
+            for (int j = 0; j < abc.length; j++) {
+                System.out.println("abc[" + i + "][" + j + "] = " + abc[i][j]);
+            }
+        }
     }
 }
